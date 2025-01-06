e0aa2af01db2e3f014270639cc11e21ff11b12b8
Raquel Alvarado <zilpa.alvarado@unah.hn>
2025-01-06 14:38:22 -0600

    mostrando el consumo en la interfaz

--- src/components/OrdenContents.tsx
+++ src/components/OrdenContents.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../helpers"
 import { OrderItem } from "../types"
 
 type OrdenContentsProps = {
@@ -9,14 +10,21 @@ export default function OrdenContents({order} : OrdenContentsProps) {
   return (
     <div>
         <h2 className='font-black text-4xl'>Consumo</h2>
-        <div className=" space-y-3 mt-5">
+        <div className=" space-y-3 mt-10">
             {order.length === 0 ? 
             <p className="text-center">La orden esta Vacia </p>
             : (order.map(item =>(
-                <div className="" key={item.id}>
-                    <p>
-                        {item.name}
+                <div className="flex justify-between border-t border-gray-200 py-5 last-of-type:border-b items-center" key={item.id}>
+                    <div className="">
+                    <p className="text-lg">
+                        {item.name} - { formatCurrency(item.price)}
                     </p>
+                    <p className="font-black">
+                       Cantidad: {item.quantity} - {
+                        formatCurrency(item.price * item.quantity) }
+                    </p>
+                    </div>
+                    <button className="bg-red-600 h-8 w-8 rounded-full text-white font-black">X</button>
                 </div>
             )))}
         </div>
