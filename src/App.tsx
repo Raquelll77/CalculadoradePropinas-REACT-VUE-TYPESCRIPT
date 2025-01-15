e5bdd8b38c1d688d7d96d30c8af756f592d60c28
Raquel Alvarado <zilpa.alvarado@unah.hn>
2025-01-15 09:37:45 -0600

    cambiando de useState a UseReducer

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import useOrder from "./hooks/useOrder"
 import OrdenContents from "./components/OrdenContents"
 import OrderTotals from "./components/OrderTotals"
 import TipPercentageForm from "./components/TipPercentageForm"
+import { useReducer } from "react"
+import { initialState, orderReducer } from "./reducers/order-reducer"
 function App() {
 
-  const {order, addItem, removeItem, tip, setTip, placeOrder } = useOrder() 
+  const {order, removeItem, tip, setTip, placeOrder } = useOrder()
+  
+  const [state, dispatch] = useReducer(orderReducer, initialState)
 
   return (
     <>
@@ -22,17 +26,17 @@ function App() {
               <MenuItem
                 key={item.id}
                 item={item}
-                addItem={addItem}
+                dispatch={dispatch}
               />
             ))}
           </div>
           
         </div>
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10"> 
-          {order.length > 0 ? (
+          {state.order.length > 0 ? (
             <>
             <OrdenContents 
-            order = {order}
+            order = {state.order}
             removeItem = {removeItem} />
 
             <TipPercentageForm 
@@ -41,7 +45,7 @@ function App() {
             />
             
             <OrderTotals 
-            order = {order}
+            order = {state.order}
             tip = {tip}
             placeOrder = {placeOrder}
             />
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,13 +1,16 @@
 import type { MenuItem } from "../types"
+import { OrderActions } from "../reducers/order-reducer"
+import { Dispatch } from "react"
+
 type MenuItemProps = {
     item: MenuItem
-    addItem: (item : MenuItem) => void
+    dispatch: Dispatch<OrderActions>
 }
 
-export default function MenuItem({item, addItem} : MenuItemProps){
+export default function MenuItem({item, dispatch} : MenuItemProps){
   return (
     <button
-    className="border-2 border-teal-400 w-full p-3 flex justify-between hover:bg-teal-200 rounded-md" onClick={()=>addItem(item)}>
+    className="border-2 border-teal-400 w-full p-3 flex justify-between hover:bg-teal-200 rounded-md" onClick={()=>dispatch({type: 'add-item', payload: {item} })}>
         <p>{item.name}</p>
         <p className="font-black">${item.price}</p>
     </button>
